6aa1e0bba8156c51ce1859f0b9c4a7f10da9ea92
walltim3 <commonuserwalltime@gmail.com>
2022-03-31 22:29:05 +0300

    first commit

---
+++ lab_1/gatherRss/app/Main.hs
@@ -0,0 +1,46 @@
+module Main where
+
+import Control.Monad
+import Data.List (find)
+import Data.Maybe
+import Network.HTTP
+import Network.Stream
+import System.Exit
+import System.Environment
+import System.IO as SIO
+import Text.XML.Light
+import Data.ByteString.Char8 as UC (putStrLn, pack, writeFile)
+import Main.Utf8
+import Data.List.Split
+import System.Directory (createDirectoryIfMissing)
+
+
+readDataFrom fileHandle = 
+  do 
+    isFileEnd <- hIsEOF fileHandle
+    if isFileEnd 
+      then
+        return ("")
+      else
+        do
+          url <- hGetLine fileHandle
+          writeRss $ url
+          readDataFrom fileHandle
+
+
+writeRss url = 
+    do 
+      resp <- simpleHTTP (getRequest url)
+      code <- getResponseCode resp
+      if code == (2,0,0)
+        then do
+          body <- getResponseBody resp
+          createDirectoryIfMissing True $ "./RSS_FEEDS"
+          UC.writeFile ("./RSS_FEEDS/" ++ last(splitOn "/" url)) (UC.pack $ body)
+        else
+          UC.writeFile ("./RSS_FEEDS/" ++last(splitOn "/" url)) (UC.pack $ "sdfvg")
+
+    
+main = withUtf8 $ do
+  fileHandle <- openFile "rssFeeds.txt" ReadMode
+  readDataFrom fileHandle
